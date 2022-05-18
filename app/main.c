119a77796e5a9304360968380db6504e3d631051
DaniloPeter <gomerplayer@gmail.com>
2022-05-18 15:10:09 +0300

    first commit

---
+++ app/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#include "myfunc.h"
+
+int main()
+{
+    double x1 = 0;
+    double x2 = 0;
+    int p = SolveEq(1, 3, 2, &x1, &x2);
+    if (!p)
+        printf("%f, %f\n", x1, x2);
+    else if (p == 1)
+        printf("No roots\n");
+    else
+        printf("%f\n", x1);
+}
---
+++ tests/mytest.h
@@ -0,0 +1,64 @@
+#ifndef MYTEST_H
+#define MYTEST_H
+#include <gtest/gtest.h>
+#include <gmock/gmock-matchers.h>
+
+extern "C"{
+#include "myfunc.h"
+}
+
+TEST(noroots_TEST, suite1)
+{
+    double x1;
+    double x2;
+    int c = SolveEq(0, 0, 3, &x1, &x2);
+    EXPECT_EQ(c, 1);
+}
+
+TEST(noroots_TEST, suite2)
+{
+    double x1;
+    double x2;
+    int c = SolveEq(0, 0, 0, &x1, &x2);
+    EXPECT_EQ(c, 1);
+}
+
+TEST(noroots_TEST, suite3)
+{
+    double x1;
+    double x2;
+    int c = SolveEq(1, 2, 3, &x1, &x2);
+    EXPECT_EQ(c, 1);
+}
+
+TEST(oneroot_TEST, suite1)
+{
+    double x1;
+    double x2;
+    int c = SolveEq(0, 2, 4, &x1, &x2);
+    EXPECT_EQ(c, 2);
+    EXPECT_EQ(x1, -2);
+}
+
+TEST(oneroot_TEST, suite2)
+{
+    double x1;
+    double x2;
+    int c = SolveEq(1, 2, 1, &x1, &x2);
+    EXPECT_EQ(c, 2);
+    EXPECT_EQ(x1, -1);
+}
+
+
+TEST(tworoots_TEST, suite1)
+{
+    double x1;
+    double x2;
+    int c = SolveEq(1, 2, -3, &x1, &x2);
+    EXPECT_EQ(c, 0);
+    EXPECT_EQ(x1, 1);
+    EXPECT_EQ(x2, -3);
+}
+
+
+#endif // MYTEST_H
